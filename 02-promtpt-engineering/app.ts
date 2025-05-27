0270ffcfa00863fa711a06bd6e03b522f610b028
Usama Ayoub <usamabinayoub@gmail.com>
2025-05-27 17:36:21 +0500

    added changes

--- 02-promtpt-engineering/app.ts
+++ 02-promtpt-engineering/app.ts
@@ -1,24 +1,19 @@
-import * as process from "node:process";
-import { AzureOpenAI as OpenAI } from "openai";
+import process from "node:process"
+import {AzureOpenAI as OpenAI} from "openai"
 
-async function main() {
-  const prompt = `Assistant helps the user with questions
+const prompt = `Assistant helps the user with questions
 
 # Question
 What shoes should I use for running?
-`;
+`
 
-  const openai = new OpenAI();
-
-  const chunks = await openai.chat.completions.create({
-    messages: [{ role: "user", content: prompt }],
-    model: "gpt-4o",
+const openai = new OpenAI()
+const chunks = await openai.chat.completions.create({
+    messages: [{role: "user", content: prompt}],
+    model: "gpt-4.0",
     stream: true,
-  });
+})
 
-  for await (const chunk of chunks) {
-    process.stdout.write(chunk.choices[0]?.delta?.content ?? "");
-  }
+for await (const chunk of chunks){
+    process.stdout.write(chunk.choices[0]?.delta?.content ?? "")
 }
-
-main().catch(console.error);
